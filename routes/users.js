15496f5adf31a25513f598c1973fbd6702078147
x1iaox1iao00 <1534971622@qq.com>
2018-11-10 13:13:37 +0800

    账号管理页面后台数据交互

--- routes/users.js
+++ routes/users.js
@@ -21,9 +21,11 @@ router.post('/add', function(req, res, next) {
 
     //对密码进行md5加密
     pass=md5.createHash("md5").update(pass).digest("hex");
-    let sqlStr=`insert into userTable(userName,userPwd,userGroup) values('${username}','${pass}','${region}')`;
+    let sqlParams=[username,pass,region]; //参数数组
+    let sqlStr="insert into userTable(userName,userPwd,userGroup) values(?,?,?)"; //占位符
+
     //执行语句
-    connection.query(sqlStr, function (error, results) {
+    connection.query(sqlStr,sqlParams, function (error, results) {
         if (error) throw error;
         else{
         if(results.affectedRows>0){
@@ -33,6 +35,18 @@ router.post('/add', function(req, res, next) {
 
         }
         }
+    });
+});
+// 获取用户列表的路由
+router.get("/list",(req,res)=>{
+    //1. 构造sql语句
+    let sqlStr="select * from userTable order by u_id DESC";
+
+    //2. 执行sql语句
+    connection.query(sqlStr,function (error,userlist) {
+        if (error) throw error; //出错后面的代码不执行
+        //3. 返回查询的结果到前端（对象数组）
+        res.send(userlist);
     });
 });
 
